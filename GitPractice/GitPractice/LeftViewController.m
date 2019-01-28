e612486257e543552eb9b6172227efcdbbc6bd1b
陈芳军 <84005870@qq.com>
2019-01-28 10:19:28 +0800

    no message

---
+++ GitPractice/GitPractice/LeftViewController.m
@@ -0,0 +1,32 @@
+//
+//  LeftViewController.m
+//  GitPractice
+//
+//  Created by 树妖 on 2019/1/28.
+//  Copyright © 2019年 树妖. All rights reserved.
+//
+
+#import "LeftViewController.h"
+
+@interface LeftViewController ()
+
+@end
+
+@implementation LeftViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ GitPractice/GitPractice/RightViewController.h
@@ -0,0 +1,17 @@
+//
+//  RightViewController.h
+//  GitPractice
+//
+//  Created by 树妖 on 2019/1/28.
+//  Copyright © 2019年 树妖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RightViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
